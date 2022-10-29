02ce1ad8d98d956966af055a8f14a8ccffeda404
ivorebeloo <imsro@iscte-iul.pt>
2022-10-29 22:50:47 +0100

    Inicial commit

---
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/App.java
@@ -0,0 +1,13 @@
+package ES_LETI_1Sem_2022_Grupo_9.ESGrupo9;
+
+/**
+ * Hello world!
+ *g
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

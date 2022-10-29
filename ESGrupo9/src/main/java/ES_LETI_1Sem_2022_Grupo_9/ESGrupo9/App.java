dd4282a98393279b4e06ebadf1827f10771e5b0a
ivorebeloo <imsro@iscte-iul.pt>
2022-10-29 23:58:47 +0100

    Versão Inicial do Trabalho

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/App.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/App.java
@@ -1,10 +1,9 @@
 package ES_LETI_1Sem_2022_Grupo_9.ESGrupo9;
 
-public class App 
-{
+public class App {
+	
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
     
-    // Test 2
 }

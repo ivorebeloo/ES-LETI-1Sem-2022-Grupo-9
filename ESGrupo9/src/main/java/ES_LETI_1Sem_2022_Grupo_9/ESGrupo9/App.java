93206500a1cdb4a6b8be905648f65e54197c09a2
ivorebeloo <imsro@iscte-iul.pt>
2022-10-29 23:35:15 +0100

    Teste

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/App.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/App.java
@@ -1,8 +1,8 @@
 package ES_LETI_1Sem_2022_Grupo_9.ESGrupo9;
 
-public class App 
-{
+public class App {
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
+    // teste
 }

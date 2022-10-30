ef3592843985e446d2604b301713a55a2c4c9af8
Leandro_Neves <lsnso@iscte-iul.pt>
2022-10-30 19:51:17 +0000

    get http codigo

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
@@ -1,12 +1,28 @@
 package ES_LETI_1Sem_2022_Grupo_9.ESGrupo9;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.HttpsURLConnection;
 
 public class Calendar {
 
-	public static void getCalendar(String[] args) {
-		String callendarURL = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?\r\nmethod=iCalendar&username=imsro@iscte.pt&password=aXWvsniEJIyWHxKZ4X4VMovKuXhJEAt7j1u450VOnlnp28QCJIhhwZWCaIP4CWrOGFpden1pjFDY3qQjO549FL3EMNv3lEpXQopNsMJdHdlkIAkGSFmnbYV0LD4Dziwg";
-		URL url = new URL(callendarURL);
-		
+	public static void getCalendar(String[] args) throws Exception { 
+		String calendarURL = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?\r\nmethod=iCalendar&username=imsro@iscte.pt&password=aXWvsniEJIyWHxKZ4X4VMovKuXhJEAt7j1u450VOnlnp28QCJIhhwZWCaIP4CWrOGFpden1pjFDY3qQjO549FL3EMNv3lEpXQopNsMJdHdlkIAkGSFmnbYV0LD4Dziwg";
+		URL url = new URL(calendarURL);
+		HttpURLConnection conexao= (HttpURLConnection) url.openConnection();
+        InputStream is = conexao.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String inputLine;
+        String jsonString = "J";
+        boolean isInsideEvent = false; 
+        while ((inputLine = br.readLine()) != null ) {
+        	if(inputLine.compareTo("BEGINEVENT")==0) { jsonString = jsonString + "J"; isInsideEvent = true; }
+        	else if(inputLine.compareTo("ENDEVENT")==0 && isInsideEvent) { jsonString = jsonString + inputLine + "J"; }
+        }
+       jsonString = "J";
+       System.out.println(jsonString);
+       br.close(); 
 	}
 
 

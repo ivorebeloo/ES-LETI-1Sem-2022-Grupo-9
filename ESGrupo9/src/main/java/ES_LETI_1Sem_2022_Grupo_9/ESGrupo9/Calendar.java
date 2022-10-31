d5e7204857cacd5b5dc113edb25f822c0ca51ff9
ivorebeloo <imsro@iscte-iul.pt>
2022-10-31 15:49:47 +0000

    Pequena atualização

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
@@ -5,26 +5,31 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class Calendar {
 
-	public static void getCalendar(String[] args) throws Exception { 
-		String calendarURL = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?\r\nmethod=iCalendar&username=imsro@iscte.pt&password=aXWvsniEJIyWHxKZ4X4VMovKuXhJEAt7j1u450VOnlnp28QCJIhhwZWCaIP4CWrOGFpden1pjFDY3qQjO549FL3EMNv3lEpXQopNsMJdHdlkIAkGSFmnbYV0LD4Dziwg";
+	public static void getCalendar() throws Exception {
+		String calendarURL = "http://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=imsro@iscte.pt&password=aXWvsniEJIyWHxKZ4X4VMovKuXhJEAt7j1u450VOnlnp28QCJIhhwZWCaIP4CWrOGFpden1pjFDY3qQjO549FL3EMNv3lEpXQopNsMJdHdlkIAkGSFmnbYV0LD4Dziwg";
 		URL url = new URL(calendarURL);
-		HttpURLConnection conexao= (HttpURLConnection) url.openConnection();
-        InputStream is = conexao.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        
-        String inputLine;
-        String jsonString = "J";
-        boolean isInsideEvent = false; 
-        while ((inputLine = br.readLine()) != null ) {
-        	if(inputLine.compareTo("BEGINEVENT")==0) { jsonString = jsonString + "J"; isInsideEvent = true; }
-        	else if(inputLine.compareTo("ENDEVENT")==0 && isInsideEvent) { jsonString = jsonString + inputLine + "J"; }
-        }
-       jsonString = "J";
-       System.out.println(jsonString);
-       br.close(); 
+		HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+		InputStream is = conexao.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String inputLine;
+		String jsonString = "J";
+		boolean isInsideEvent = false; 
+		while((inputLine = br.readLine()) != null) {
+			if(inputLine.compareTo("BEGINEVENT") == 0) {
+				jsonString = jsonString + "J";
+				isInsideEvent = true;
+			} else if(inputLine.compareTo("ENDEVENT") == 0 && isInsideEvent) {
+				jsonString = jsonString + inputLine + "J";
+			}
+		}
+		jsonString = calendarURL;
+		System.out.println(jsonString);
+		br.close(); 
 	}
 
-
+	public static void main(String[] args) throws Exception {
+		getCalendar();
+	}
 
 }

7bb442e9b57f677b27f5a1116b42743abf77dbb1
Leandro_Neves <lsnso@iscte-iul.pt>
2022-11-02 22:36:39 +0000

    Calendario convertido para JSON

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
@@ -14,12 +14,16 @@ public class Calendar {
 		URL calendarURL = new URL("https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=imsro@iscte.pt&password=aXWvsniEJIyWHxKZ4X4VMovKuXhJEAt7j1u450VOnlnp28QCJIhhwZWCaIP4CWrOGFpden1pjFDY3qQjO549FL3EMNv3lEpXQopNsMJdHdlkIAkGSFmnbYV0LD4Dziwg");
 		URLConnection URL = calendarURL.openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(URL.getInputStream()));
+		File file = new File("JSONCalendar/" + "Calendar"+ ".json");
 		String inputLine;
-		while ((inputLine = br.readLine()) != null) 
-			System.out.println(new Gson().toJson(inputLine));
+		FileWriter fileWriter = new FileWriter(file,false);
+		while ((inputLine = br.readLine()) != null) {
+			fileWriter.write(new Gson().toJson(inputLine) + "\n");
 			//System.out.println(inputLine);
+	}
+		fileWriter.close();
 		br.close();
-		
+		System.out.println("JA ACABEI");
 	}
 	
 	public static void main(String[] args) throws Exception {

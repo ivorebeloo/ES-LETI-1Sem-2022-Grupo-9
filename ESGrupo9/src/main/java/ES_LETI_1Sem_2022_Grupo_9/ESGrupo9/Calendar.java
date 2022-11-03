5a404b1480dfc8808006699e2af7f515bee312e2
marisaiscte <mmsas2@iscte-iul.pt>
2022-11-03 00:46:34 +0000

    Alteração

--- ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
+++ ESGrupo9/src/main/java/ES_LETI_1Sem_2022_Grupo_9/ESGrupo9/Calendar.java
@@ -11,52 +11,46 @@ import com.google.gson.Gson;
 public class Calendar {
 
 	public static void getCalendar() throws Exception {
-		
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Insira aqui o seu URI:" + "\n");
-	
-		//0System.out.println("------------------------------NOVA LEITURA------------------------------------------------");	
-		
+		System.out.println("Insira aqui o seu URI:" + "\n");	
 		String inputLine =sc.nextLine();
 		sc.close();
-		
-//		System.out.println("Insira aqui o seu nome:" + "\n");
-//		Scanner scanner = new Scanner(System.in);
-//		String nome= scanner.nextLine();
-//		String nomeSemEspaco=nome.replaceAll("\\s+","");
-//		scanner.close();
-		
+
+		//		System.out.println("Insira aqui o seu nome:" + "\n");
+		//		Scanner scanner = new Scanner(System.in);
+		//		String nome= scanner.nextLine();
+		//		String nomeSemEspaco=nome.replaceAll("\\s+","");
+		//		scanner.close();
+
 		URL calendarURL = new URL(inputLine);
 		URLConnection URL = calendarURL.openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(URL.getInputStream()));
 
-		
-	//	File file = new File("JSONCalendar/" + nomeSemEspaco+"URL"+ ".json");
-		File file = new File("JSONCalendar/" +"Calendar"+ ".json");
 
+		//	File file = new File("JSONCalendar/" + nomeSemEspaco+"URL"+ ".json");
+		File file = new File("JSONCalendar/" +"Calendar"+ ".json");
 		if (!file.exists()){
 			file.createNewFile();
 		}	
+
 		FileWriter fileWriter = new FileWriter(file,false);
-		
+
 		while ((inputLine = br.readLine()) != null) {
-			fileWriter.write(inputLine + "\n");
+			//fileWriter.write(inputLine + "\n");
+			fileWriter.write(new Gson().toJson(inputLine) + "\n");
 		}
 
-//		while ((inputLine = br.readLine()) != null) {
-//			fileWriter.write(new Gson().toJson(inputLine) + "\n");
-//			//System.out.println(inputLine);
-//	}
 		fileWriter.close();
 		br.close();
 		System.out.println("JA ACABEI");
 
 	}
-		
-	
+
+
 	public static void main(String[] args) throws Exception {
-        getCalendar();
-    }
+		getCalendar();
+	}
 
 
 
